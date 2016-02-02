73b66a2f04efd82fd882ca2f5e1fc101447407f8
bregord <sirbrendang@gmail.com>
2016-02-01 22:08:07 -0500

    first draft of tpe checker almost ALMOST done

--- a1/brendan_gordon/src/MiniParser.hs
+++ a1/brendan_gordon/src/MiniParser.hs
@@ -3,52 +3,38 @@ module Main where
 import System.Environment
 import System.IO
 import Control.Monad
+import Control.Monad.Error
 import Text.ParserCombinators.Parsec
 import Text.ParserCombinators.Parsec.Expr
 import Text.ParserCombinators.Parsec.Language
 import Text.ParserCombinators.Parsec.Char
 import qualified Text.ParserCombinators.Parsec.Token as Token
 
---Grammar
-{-program ::= declarationsList statementsList | [] statementsList | declarationsList []
-
-declarationsList ::= declaration | declarationsList declaration
-
-declaration ::= var id colon intType semicolon | var id colon floatType semicolon | var id colon stringType semicolon 
-
-statementsList ::= statement | statementsList statement
-
-statement ::= if id then statementsList | if id then statementsList else statementsList endif | while id do statementsList done | print line | read line | id = exp
-
-line ::= string | exp
-
-exp ::= exp * exp | exp + exp | exp - exp | exp / exp | - exp | int | float | id -}
-
-
 data BinOp = Add | Multiply | Subtract | Divide deriving(Eq,Show)
 
---data UnOp = Neg deriving(Eq,Show)
-
 type Id = String 
-
---data Id =  String deriving(Eq,Show)
 
 data LineStmt = StringLine String | IdString Id deriving(Eq,Show)
 
 data Decl = DecSeq [Decl] | Dec Id Type  deriving(Eq,Show)
 
-data Stmt = Seq [Stmt] | If Id [Stmt] | IfElse Id [Stmt] [Stmt] | While Id [Stmt] | Read Expr | Print Expr | Expr |IdStmt String | Assn Id Expr deriving(Eq,Show)
+data Stmt = Seq [Stmt] | If Expr [Stmt] | IfElse Expr [Stmt] [Stmt] | While Expr [Stmt] | Read Expr | Print Expr | Expr |IdStmt String | Assn Id Expr deriving(Eq,Show)
 
 data Expr = Var String | IntConst Integer | FloatConst Double| Binary BinOp Expr Expr | Neg Expr | StringEx String  deriving(Eq,Show)
 
-data Type = FloatType String | IntType String | StringType String deriving (Eq,Show)
+data Type = FloatType| IntType | StringType | NullType deriving (Eq,Show)
 
+data Program = Program [Decl] [Stmt] deriving(Eq,Show)
+
+data AssocMap = AssocMap (Id, Type) deriving(Eq,Show)
+
+data CheckError = CheckError String deriving(Eq, Show)
 
 languageDef = 
     emptyDef{
            Token.commentLine     = "#"
-           , Token.identStart = letter
-           ,Token.identLetter = alphaNum
+           , Token.identStart = letter <|> char '_'
+           ,Token.identLetter = alphaNum <|> char '_'
           , Token.reservedNames   = [ "if"
                                     , "then"
                                     , "else"
@@ -59,6 +45,9 @@ languageDef =
                                     , "var"
                                     , "read"
                                     ,"print"
+                                    , "int"
+                                    , "float"
+                                    , "string"
                                     ]
           , Token.reservedOpNames = ["+", "-", "*", "/", "="
           ]
@@ -79,8 +68,18 @@ whiteSpace = Token.whiteSpace lexer -- parses whitespace
 parens = Token.parens lexer
 dot = Token.dot lexer
 
-miniParser:: Parser [Stmt]
-miniParser = whiteSpace >> many declaration >> many statement
+
+miniParser:: Parser Program
+miniParser = 
+    do 
+        whiteSpace 
+        x <- many declaration 
+        y <- many statement
+        return $ Program  x y
+
+--miniParser:: Parser [Stmt]
+--miniParser = whiteSpace >> many declaration >> many statement
+--Compose miniParser with another to get declarations too.
 
 statement :: Parser Stmt
 statement = ifStmt
@@ -96,7 +95,7 @@ declaration = decStmt
 ifStmt :: Parser Stmt
 ifStmt =
   do reserved "if"
-     cond  <-identifier 
+     cond  <-expression
      reserved "then"
      stmt1 <- many statement
      reserved "endif"
@@ -106,7 +105,7 @@ ifStmt =
 ifElseStmt :: Parser Stmt
 ifElseStmt =
   do reserved "if"
-     cond  <- identifier 
+     cond  <- expression 
      reserved "then"
      stmt1 <- many statement
      reserved "else"
@@ -116,7 +115,7 @@ ifElseStmt =
 whileStmt :: Parser Stmt
 whileStmt =
   do reserved "while"
-     cond <- identifier 
+     cond <- expression 
      reserved "do"
      stmt <- many statement
      reserved "done"
@@ -156,17 +155,17 @@ typer = stringType <|> intType <|> floatType
 stringType:: Parser Type
 stringType = do
     val <- reserved "string"
-    return $ StringType "string"
+    return $ StringType
 
 floatType:: Parser Type
 floatType = do
     val <- reserved "float"
-    return $ FloatType "float"
+    return $ FloatType 
 
 intType:: Parser Type
 intType = do
     val <- reserved "int"
-    return $ IntType "int"
+    return $ IntType 
 
 decStmt :: Parser Decl
 decStmt = 
@@ -188,8 +187,6 @@ operators = [ [Prefix (reservedOp "-" >> return (Neg             ))          ]
                 Infix  (reservedOp "-"   >> return (Binary Subtract)) AssocLeft]
               ]
 
---THIS PART
-
 floatParser:: Parser Double 
 floatParser = 
     do
@@ -198,9 +195,20 @@ floatParser =
         post <- many digit
         case (pre,post) of
             ([],[]) -> fail "Not a number"--Fail
-            (n,[]) -> return $ read n
+            (n,[]) ->  if (head n) == '0' && (length n) > 1 then fail "0 error"  else return $ read n 
             ([],m) -> return $ read m
-            (n,m) -> return $ (read $ (n ++ "." ++  m)) --map show over both lists. Then concatenate it and read it.
+            (n,m) -> if (head n) == '0' && (length n) > 1 then fail "0 error" else return $ (read $ (n ++ "." ++  m)) --map show over both lists. Then concatenate it and read it.
+
+integerParser:: Parser Integer
+integerParser = 
+    do
+        num <-many digit
+        --return $ read num
+        if (head num) == '0' && (length num) > 1 then fail "0 error"  else return $ read num
+       -- case num of
+        --0:[] -> return 0
+        --0:_ -> fail
+        --[_] -> return num
 
 letters = ['0'..'9']++['a'..'z']++['A'..'Z']++[',','.','!','!',' ']
 
@@ -216,24 +224,175 @@ term::Parser Expr
 term = parens expression  
     <|>liftM Var identifier
     <|> try ( liftM FloatConst floatParser) 
-    <|> (liftM IntConst integer)
+    <|> try (liftM IntConst integerParser)
     <|> liftM StringEx stringParser
     -- <|> liftM FloatConst float --FloatConst Double 
     --get as a list of numbers, and just zip with increasing powers of 10 to actually make it a number
 
 
-parseString :: String -> [Stmt]
+--Pretty Printer
+class PrettyPrint a where
+    prettyPrint::a->String
+
+instance PrettyPrint Expr where
+    prettyPrint (Neg a) = ("-" ++ (prettyPrint a)) 
+    prettyPrint (Var a) =  a
+    prettyPrint (IntConst a) = show a
+    prettyPrint (FloatConst a) = show a
+    prettyPrint (StringEx a) = a
+    prettyPrint (Binary op a b) =  read $ (prettyPrint a) ++ (prettyPrint op) ++ (prettyPrint b)
+
+instance PrettyPrint Stmt where
+    prettyPrint (Seq a) = prettyPrint a
+    prettyPrint (If a b) =  "if " ++ (prettyPrint a) ++ " then " ++ (prettyPrint b) 
+    prettyPrint (IfElse a b c) = "if " ++ (prettyPrint a) ++ " then " ++ (prettyPrint b) ++ " else " ++ (prettyPrint c)
+    prettyPrint (While a b) =  "while " ++ (prettyPrint a) ++ " do " ++ (prettyPrint b) ++ " done" 
+    prettyPrint (Print a) = prettyPrint a ++ ";"
+    prettyPrint (Read a) =  prettyPrint a ++ ";"
+    prettyPrint (Assn a b) =  a ++ " = " ++  (prettyPrint b) ++ ";"
+    prettyPrint (IdStmt a) = a
+
+instance PrettyPrint a => (PrettyPrint [a]) where
+    prettyPrint a = concat $ map prettyPrint a
+
+instance PrettyPrint BinOp where
+    prettyPrint Add = "+"
+    prettyPrint Multiply = "*"
+    prettyPrint Subtract = "-"
+    prettyPrint Divide = "/"
+
+instance PrettyPrint Decl where
+    prettyPrint (DecSeq a) = prettyPrint a
+    prettyPrint (Dec a b) =  "var " ++ (a) ++ ": " ++ prettyPrint b ++ ";"
+
+instance PrettyPrint Type where
+    prettyPrint (FloatType) = "float"
+    prettyPrint (IntType) = "int"
+    prettyPrint (StringType) = "string" 
+
+instance PrettyPrint Program where
+    prettyPrint (Program a b) = (prettyPrint a) ++ (prettyPrint b)
+
+instance PrettyPrint AssocMap where
+    prettyPrint (AssocMap (a, b)) = a ++ ":" ++ (prettyPrint b) ++ "\n" 
+
+
+
+pretty::(PrettyPrint a)=>a->String
+pretty a = prettyPrint a
+
+parseString :: String -> Program
 parseString str =
   case parse (miniParser  <* eof) "" str of
     Left e  -> error $ show e
-    Right r -> r
+    Right r ->  r
 
 parseFile :: String -> IO () 
 parseFile file =
   do program  <- readFile file
      case parse (miniParser <* eof) file program   of
        Left e -> do putStrLn "Invalid" >> print e 
-       Right r -> do putStrLn "Valid"
+       Right r -> do putStrLn "Valid" >> print r
+
+
+--Type Checking
+symAdd::Decl->AssocMap
+symAdd (Dec a b) = AssocMap(a,b)
+
+--if a stmt is used in a different context than it should be, return false. otherwise true.
+--checkValidity::[AssocMap]->[Stmt]->Bool
+
+--Either type?
+--I NEED SOMET YPE CHECKING OF SOMESORT
+
+
+--CHECK IF IN THE MAP, AND THEN CHECK TO SEE IF THEY SATISFY
+
+--Want to make sure types match up.
+typeCheckStmt::[AssocMap]->Stmt->Bool
+typeCheckStmt [] _ =  False --ERROR - Clearly the thing is not declared 
+typeCheckStmt m (Assn a b) = if isInMap m a then typeCheckExpr m b else False 
+typeCheckStmt m (If a b)  = if (isInMap m a) && (typeCheckExpr m a) == IntType && (getTypeFromMap m a) == IntType then typeCheckExpr m b else False
+typeCheckStmt m (IfElse a b c) = if isInMap m a && (typeCheckExpr m a)==IntType && (getTypeFromMap m a) ==IntType then typeCheckExpr m b && typeCheckExpr m c else False
+typeCheckStmt m (While a b) = if isInMap m a && (typeCheckExpr m a)==IntType && (getTypeFromMap m a) ==IntType then typeCheckExpr m b else False
+
+typeCheckExpr::[AssocMap]->Expr->Either CheckError Type
+typeCheckExpr m (Var a) = getTypeFromMap m a --get from the list and return that 
+typeCheckExpr m (IntConst a) = Right IntType
+typeCheckExpr m (FloatConst a) = Right FloatType
+typeCheckExpr m (Binary op a b) = typeCheckBinOp m op a b
+typeCheckExpr m (Neg a) = typeCheckExpr m a 
+typeCheckExpr m (StringEx a) = Right StringType
+
+--this is where most rules come into play
+
+--DO AN EITHER STATEMENT HERE
+typeCheckBinOp::[AssocMap]->BinOp->Expr->Expr->Either CheckError Type
+typeCheckBinOp m op lt rt =
+    do 
+        lhs <- typeCheckExpr m lt
+        rhs <- typeCheckExpr m rt
+        case (op, lhs,rhs) of
+			(Add, IntType, IntType) -> Right IntType 
+			(Add, IntType, FloatType)-> Right FloatType 
+			(Add, FloatType, IntType)-> Right FloatType 
+			(Add, FloatType, FloatType)-> Right FloatType 
+			(Add, StringType, StringType)-> Right StringType 
+			(Add, _, _)-> Left (CheckError "Invalid Type Combination") 
+			(Subtract, IntType, IntType) -> IntType 
+			(Subtract, IntType, FloatType) -> Right FloatType 
+			(Subtract, FloatType, IntType) -> Right  FloatType 
+			(Subtract, FloatType, FloatType) -> Right FloatType 
+			(Subtract, StringType, StringType) -> Right  StringType
+			(Subtract, _, _) -> Left (CheckError "Invalid Type Combination")
+			(Divide, IntType, IntType) -> Right IntType 
+			(Divide, IntType, FloatType) -> Right FloatType 
+			(Divide, FloatType, IntType) -> Right FloatType 
+			(Divide, FloatType, FloatType) -> Right FloatType 
+			(Divide, _, _) -> Left (CheckError "Invalid Type Combination")
+			(Multiply, IntType, IntType) -> Right IntType 
+			(Multiply, IntType, FloatType) -> Right FloatType 
+			(Multiply, FloatType, IntType) -> Right FloatType 
+			(Multiply, FloatType, FloatType) -> Right FloatType 
+			(Multiply, _, _) -> Left (CheckError "Invalid Type Combination")
+			(_,_,_) -> Left (CheckError "Invalid Type")
+
+{-
+redec::String->IO ()
+redec fileName =
+    do
+        handle <- openFile fileName ReadMode
+        let contents <- hGetContents handle -- gets a list of "ID : Type"
+        let assocMap = map stringToAssocMap contents
+        hClose handle
+        return
+        --get each association, and build it
+        --check the associations
+--Build AssocMap
+--check It
+
+-}
+
+getTypeFromMap::[AssocMap]->Id->Either CheckError Type
+getTypeFromMap [] a = Left (CheckError "Type Not Found") --Or maybe it should fail 
+getTypeFromMap ((i,t):xs) a = if i == a then Right t else (getTypeFromMap a xs)
+
+
+--ERROR HANDLING
+isInMap::[AssocMap]->Id->Bool
+isInMap m a = if (length (filter ((==a).fst) m) ==1) then True else False 
+
+typeCheck::Program->String->IO Bool
+typeCheck (Program a b) fileName = 
+    do
+        --let handle = (many (noneOf "." fileName)) -- ++ ".symbol.txt"
+        handle <- openFile (fileName ++ ".symbol.txt") WriteMode
+        let assocMapList = map symAdd a --list of maps
+        --
+        let b = map pretty assocMapList
+        hPutStr handle $ concat b
+        hClose handle
+        return True
 
 main = do
     (arg:_) <- getArgs 
